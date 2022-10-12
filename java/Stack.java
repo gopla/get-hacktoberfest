88ea9a6df5cba35c11d8604ab38a93fffefcdcfc
bagusp3 <bagus.chmod@gmail.com>
2022-10-12 16:16:35 +0700

    Stack in java

---
+++ java/Stack.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stack;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class Stack {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
